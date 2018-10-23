ea4b9a5cd7ad21d2de08a10dfeb3892637374c21
MiJooJeong <uuuio1224@gmail.com>
2018-10-23 20:32:34 +0900

    CommentForm에 영어환자 validator 추가

--- blog/forms.py
+++ blog/forms.py
@@ -1,3 +1,5 @@
+import re
+
 from django import forms
 from blog.models import Comment
 
@@ -6,3 +8,12 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['message']
+
+    def clean_message(self):
+        message = self.cleaned_data.get('message', None)
+        if message:
+            if not re.search(r'[ㄱ-힣]', message):
+                raise forms.ValidationError('메세지에 한글이 필요합니다.')
+
+        return message
+    
